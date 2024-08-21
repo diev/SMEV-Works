48dc613d884a29e3978bc5850ceded6417649493
Dmitrii Evdokimov <diev@mail.ru>
2024-08-21 14:00:35 +0300

    Update ERROR notification

--- XslTrans/samples/adapter_in.cmd
+++ XslTrans/samples/adapter_in.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem Edition 2023-05-31
+rem Edition 2024-08-21
 
 rem echo %date%
 rem 31.05.2023
@@ -29,6 +29,10 @@ goto :eof
 rem ----------------------------------------------
 :switch
 echo %1
+
+find ":NALFLPROCRequest" %1>nul
+if %errorlevel%==0 goto 310-71
+
 find ":UVSMERTFLRequest" %1>nul
 if %errorlevel%==0 goto 313-19
 
@@ -37,7 +41,33 @@ rem Add below other incoming requests
 rem find ":OtherRequest" %1>nul
 rem if %errorlevel%==0 goto other
 
+find "ErrorMessage" %1>nul
+if %errorlevel%==0 goto error
+
 goto unknown
+
+rem ----------------------------------------------
+:310-71
+rem Set path to Archive and a new name
+set a=%arc%\%tag%-310-71-
+
+rem Put the source file to Archive
+copy %1 %a%%~nx1
+rem Delete if success
+if exist %a%%~nx1 del %1
+
+rem Put result files in Archive
+rem Make a response file for sending back
+XslTrans\XslTrans %a%%~nx1 XslTrans\XSLT\310-71x.xslt %a%%~n1.reply.xml
+rem Make a printable report for subscribers
+XslTrans\XslTrans %a%%~nx1 XslTrans\XSLT\310-71t.xslt %a%%~n1.txt
+
+rem Do actions with result files from Archive
+rem Put the response file to send
+copy %a%%~n1.reply.xml %out%
+rem Send the printable report to subscribers
+mailer - "New SMEV:310-71" =%a%%~n1.txt %a%%~n1.txt
+goto :eof
 
 rem ----------------------------------------------
 :313-19
@@ -46,21 +76,38 @@ set a=%arc%\%tag%-313-19-
 
 rem Put the source file to Archive
 copy %1 %a%%~nx1
-
-rem Make reply file in Archive and put it to output
-XslTrans\XslTrans %1 XslTrans\XSLT\313-19a.xslt %a%%~n1.reply.xml
-copy %arc%%~n1.reply.xml %out%
-
-rem Make a printable report and send it to subscribers
-XslTrans\XslTrans %1 XslTrans\XSLT\313-19p.xslt %a%%~n1.html
-mailer - "New SMEV:313-19 Report" *%a%%~n1.txt %a%%~n1.html
-
-rem Make a data text and send it to ABS
-XslTrans\XslTrans %1 XslTrans\XSLT\313-19t.xslt %a%%~n1.txt
-mailer - "New SMEV:313-19 DATA" "Load it to ABS" %a%%~n1.txt
-
-rem Done, delete the source file
+rem Delete if success
 if exist %a%%~nx1 del %1
+
+rem Put result files in Archive
+rem Make a response file for sending back
+XslTrans\XslTrans %a%%~nx1 XslTrans\XSLT\313-19x.xslt %a%%~n1.reply.xml
+rem Make a printable report for subscribers
+XslTrans\XslTrans %a%%~nx1 XslTrans\XSLT\313-19h.xslt %a%%~n1.html
+rem Make a data text for ABS
+XslTrans\XslTrans %a%%~nx1 XslTrans\XSLT\313-19t.xslt %a%%~n1.txt
+
+rem Do actions with result files from Archive
+rem Put the response file to send
+copy %a%%~n1.reply.xml %out%
+rem Send the printable report to subscribers
+rem mailer - "New SMEV:313-19 Report for Clients Dept" =%a%%~n1.txt %a%%~n1.html
+rem Send the data text to ABS
+mailer - "New SMEV:313-19" =%a%%~n1.txt %a%%~n1.txt
+goto :eof
+
+rem ----------------------------------------------
+:error
+rem Set path to Archive and a new name
+set a=%unknown%\%tag%-error-
+
+rem Put the source file to Archive
+copy %1 %a%%~nx1
+rem Delete if success
+if exist %a%%~nx1 del %1
+
+rem Send it to our IT Lab
+mailer - "New SMEV:ERROR" *%a%%~nx1 %a%%~nx1
 goto :eof
 
 rem ----------------------------------------------
@@ -70,10 +117,9 @@ set a=%unknown%\%tag%-unknown-
 
 rem Put the source file to Archive
 copy %1 %a%%~nx1
+rem Delete if success
+if exist %a%%~nx1 del %1
 
 rem Send it to our IT Lab
-mailer - "New SMEV:UNKNOWN Request" "Unknown %a%%~nx1" %a%%~nx1
-
-rem Done, delete the source file
-if exist %a%%~nx1 del %1
+mailer - "New SMEV:UNKNOWN" *%a%%~nx1 %a%%~nx1
 goto :eof
